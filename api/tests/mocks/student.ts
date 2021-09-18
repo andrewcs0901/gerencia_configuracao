611892336a310c688906d504f4adacd59038086a
JoaoGuiMB <juaogui159@gmail.com>
2021-09-18 10:28:35 -0300

    test: update route  done

---
+++ api/tests/mocks/student.ts
@@ -0,0 +1,9 @@
+const updateStudent = {
+  id: 1,
+  name: "John Doe updated",
+  email: "john.doe@example.com",
+  city: "Belo Horizonte",
+  birth: new Date("11/13/1999"),
+};
+
+export { updateStudent };
